7a88aa41980e816be0b33cb4a38358129e49b5f8
Rob Cameron <cannikin@fastmail.com>
2024-06-10 11:04:14 -0700

    Add priority, hook up performNow() to actually call perform() immediately

--- web/src/jobs/RedwoodJob.js
+++ web/src/jobs/RedwoodJob.js
@@ -2,23 +2,27 @@ export class RedwoodJob {
   // The default queue for all jobs
   static queue = 'default'
 
+  // The default priority for all jobs
+  // Assumes a range of 1 - 100, 1 being highest priority
+  static priority = 50
+
   // Class method to schedule a job to run later
   //   const scheduleDetails = RedwoodJob.performLater('foo', 'bar')
-  static performLater = async (...args) => {
+  static performLater(...args) {
     return new this().#schedule(args)
   }
 
   // Class method to runs the job immediately in the current process
   //   const result = RedwoodJob.performNow('foo', 'bar')
-  static performNow = async (...args) => {
-    return new this().performNow(args)
+  static performNow(...args) {
+    return new this().performNow(...args)
   }
 
   // Set options on the job before enqueueing it:
   //   const job = RedwoodJob.set({ wait: 300 })
   //   job.performLater('foo', 'bar')
-  static set = (options) => {
-    return new this({ queue: this.queue, ...options })
+  static set(options) {
+    return new this({ queue: this.queue, priority: this.priority, ...options })
   }
 
   // A job can be instantiated manually, but this will also be invoked
@@ -30,20 +34,45 @@ export class RedwoodJob {
   // Instance method to schedule a job to run later
   //   const job = RedwoodJob
   //   const scheduleDetails = job.performLater('foo', 'bar')
-  async performLater(...args) {
+  performLater(...args) {
     return this.#schedule(args)
   }
 
   // Instance method to runs the job immediately in the current process
   //   const result = RedwoodJob.performNow('foo', 'bar')
-  async performNow(...args) {
-    // TODO run the job inline
-    return // job result
+  performNow(...args) {
+    return this.perform(...args)
+  }
+
+  handler(args) {
+    return {
+      class: this.constructor.name,
+      args: [...args],
+    }
+  }
+
+  // Must be implemented by the subclass
+  perform() {
+    throw new Error('You must implement the `perform` method in your job class')
   }
 
   // Determines the name of the queue
   get queue() {
-    return this.options?.queue || RedwoodJob.queue
+    return this._queue || this.options?.queue || RedwoodJob.queue
+  }
+
+  // Set the name of the queue directly on an instance of a job
+  set queue(value) {
+    this._queue = value
+  }
+
+  // Determines the priority of the job
+  get priority() {
+    return this._priority || this.options?.priority || RedwoodJob.priority
+  }
+
+  set priority(value) {
+    this._priority = value
   }
 
   // Determines when the job should run. If no options, runs as soon as possible
@@ -69,21 +98,16 @@ export class RedwoodJob {
     this._runAt = value
   }
 
-  // Must be implemented by the subclass
-  async perform() {
-    throw new Error('You must implement the `perform` method in your job class')
-  }
-
   // Schedules a job with the appropriate adapter, returns the schedule details.
   // Can't be called directly, the public interface is `performLater()`
   async #schedule(args) {
     // TODO: Actually schedule the job with the adapter
 
     return {
-      handler: this.constructor.name,
-      arguments: args,
+      handler: this.handler(args),
       runAt: this.runAt,
       queue: this.queue,
+      priority: this.priority,
     }
   }
 }
--- web/src/jobs/RedwoodJob.test.js
+++ web/src/jobs/RedwoodJob.test.js
@@ -33,14 +33,26 @@ describe('set()', () => {
     expect(job.options.queue).toEqual(RedwoodJob.queue)
   })
 
-  test('can override the queue name set in the class itself', () => {
+  test('sets the default priority', () => {
+    const job = RedwoodJob.set({ foo: 'bar' })
+
+    expect(job.options.priority).toEqual(RedwoodJob.priority)
+  })
+
+  test('can override the queue name set in the class', () => {
     const job = RedwoodJob.set({ foo: 'bar', queue: 'priority' })
 
     expect(job.options.queue).toEqual('priority')
   })
+
+  test('can override the priority set in the class', () => {
+    const job = RedwoodJob.set({ foo: 'bar', priority: 10 })
+
+    expect(job.options.priority).toEqual(10)
+  })
 })
 
-describe('runAt', () => {
+describe('get runAt()', () => {
   test('returns the current time if no options are set', () => {
     const job = new RedwoodJob()
 
@@ -71,13 +83,88 @@ describe('runAt', () => {
   })
 })
 
+describe('get queue()', () => {
+  test('defaults to queue set in class', () => {
+    const job = new RedwoodJob()
+
+    expect(job.queue).toEqual(RedwoodJob.queue)
+  })
+
+  test('can manually set the queue name on an instance', () => {
+    const job = new RedwoodJob()
+    job.queue = 'priority'
+
+    expect(job.queue).toEqual('priority')
+  })
+
+  test('queue set manually overrides queue set as an option', () => {
+    const job = RedwoodJob.set({ queue: 'priority' })
+    job.queue = 'important'
+
+    expect(job.queue).toEqual('important')
+  })
+})
+
+describe('get priority()', () => {
+  test('defaults to priority set in class', () => {
+    const job = new RedwoodJob()
+
+    expect(job.priority).toEqual(RedwoodJob.priority)
+  })
+
+  test('can manually set the priority name on an instance', () => {
+    const job = new RedwoodJob()
+    job.priority = 10
+
+    expect(job.priority).toEqual(10)
+  })
+
+  test('priority set manually overrides priority set as an option', () => {
+    const job = RedwoodJob.set({ priority: 20 })
+    job.priority = 10
+
+    expect(job.priority).toEqual(10)
+  })
+})
+
 describe('performLater()', () => {
   test('returns the properties of the scheduled job', async () => {
     const job = await RedwoodJob.performLater('foo', 'bar')
 
-    expect(job.handler).toEqual('RedwoodJob')
-    expect(job.arguments).toEqual(['foo', 'bar'])
+    expect(job.handler).toEqual({ class: 'RedwoodJob', args: ['foo', 'bar'] })
     expect(job.runAt).toEqual(new Date())
     expect(job.queue).toEqual(RedwoodJob.queue)
+  })
+})
+
+describe('instance performNow()', () => {
+  test('invokes the perform() function immediately', async () => {
+    class TestJob extends RedwoodJob {
+      async perform() {
+        return 'done'
+      }
+    }
+
+    const spy = jest.spyOn(TestJob.prototype, 'perform')
+
+    new TestJob().performNow('foo', 'bar')
+
+    expect(spy).toHaveBeenCalledWith('foo', 'bar')
+  })
+})
+
+describe('static performNow()', () => {
+  test('invokes the perform() function immediately', async () => {
+    class TestJob extends RedwoodJob {
+      async perform() {
+        return 'done'
+      }
+    }
+
+    const spy = jest.spyOn(TestJob.prototype, 'perform')
+
+    TestJob.performNow('foo', 'bar')
+
+    expect(spy).toHaveBeenCalledWith('foo', 'bar')
   })
 })
