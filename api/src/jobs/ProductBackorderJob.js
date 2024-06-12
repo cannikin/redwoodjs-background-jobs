d944d8bdecd71e1b4f1e3509f9ed1460310e4f53
Rob Cameron <cannikin@fastmail.com>
2024-06-12 12:44:35 -0700

    Example job for sending email

--- api/src/jobs/ProductBackorderJob.js
+++ api/src/jobs/ProductBackorderJob.js
@@ -1,11 +1,28 @@
 import { db } from 'src/lib/db'
+import { mailer } from 'src/lib/mailer'
+import { ProductBackorderEmail } from 'src/mail/ProductBackorderEmail'
 
 import { RedwoodJob } from './RedwoodJob'
 
 export class ProductBackorderJob extends RedwoodJob {
   // static queue = 'notifications'
+  // statis priority = 10
 
   async perform(productID) {
-    throw new Error('Could not backorder product')
+    const product = await db.product.findUnique({ where: { id: productID } })
+
+    if (product.inventoryCount > 0) {
+      const waitList = await db.waitlist.findMany({
+        where: { productId: productID },
+      })
+
+      for (const person of waitList) {
+        await mailer.send(ProductBackorderEmail({ product }), {
+          to: person.email,
+          subject: `Product Back in Stock: ${product.name}`,
+        })
+      }
+    }
+    console.log(`ProductBackorderJob: Product ${productID} is backordered`)
   }
 }
