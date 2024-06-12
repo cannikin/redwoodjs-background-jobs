a3137186d75e25d18c6c8fcb9acf967ea9f8bd97
Rob Cameron <cannikin@fastmail.com>
2024-06-12 14:51:59 -0700

    Test that the new job record is returned

--- api/src/jobs/__tests__/PrismaAdapter.test.js
+++ api/src/jobs/__tests__/PrismaAdapter.test.js
@@ -8,7 +8,7 @@ describe('schedule()', () => {
   test('creates a job in the DB', async () => {
     const adapter = new PrismaAdapter({ accessor: db.backgroundJob })
     const beforeJobCount = await db.backgroundJob.count()
-    const job = await adapter.schedule({
+    await adapter.schedule({
       handler: 'RedwoodJob',
       args: ['foo', 'bar'],
       queue: 'default',
@@ -18,9 +18,19 @@ describe('schedule()', () => {
     const afterJobCount = await db.backgroundJob.count()
 
     expect(afterJobCount).toEqual(beforeJobCount + 1)
-    expect(job.handler).toEqual(
-      JSON.stringify({ handler: 'RedwoodJob', args: ['foo', 'bar'] })
-    )
+  })
+
+  test('returns the job record that was created', async () => {
+    const adapter = new PrismaAdapter({ accessor: db.backgroundJob })
+    const job = await adapter.schedule({
+      handler: 'RedwoodJob',
+      args: ['foo', 'bar'],
+      queue: 'default',
+      priority: 50,
+      runAt: new Date(),
+    })
+
+    expect(job.handler).toEqual('{"handler":"RedwoodJob","args":["foo","bar"]}')
     expect(job.runAt).toEqual(new Date())
     expect(job.queue).toEqual('default')
     expect(job.priority).toEqual(50)
@@ -43,13 +53,15 @@ describe('schedule()', () => {
       runAt: new Date(),
     })
 
+    // definitely a different record in the DB
     expect(job1.id).not.toEqual(job2.id)
+    // but all details are identical
     expect(job1.handler).toEqual(job2.handler)
     expect(job1.queue).toEqual(job2.queue)
     expect(job1.priority).toEqual(job2.priority)
   })
 
-  test('defaults to 0 attempts', async () => {
+  test('defaults some database fields', async () => {
     const adapter = new PrismaAdapter({ accessor: db.backgroundJob })
     const job = await adapter.schedule({
       handler: 'RedwoodJob',
@@ -60,5 +72,9 @@ describe('schedule()', () => {
     })
 
     expect(job.attempts).toEqual(0)
+    expect(job.lockedAt).toBeNull()
+    expect(job.lockedBy).toBeNull()
+    expect(job.lastError).toBeNull()
+    expect(job.failedAt).toBeNull()
   })
 })
