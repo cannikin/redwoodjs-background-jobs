aca8fccd4c65cd11632fd25d9cd4273326099378
Rob Cameron <cannikin@fastmail.com>
2024-06-13 15:17:39 -0700

    Refactor, move logging to standalone function

--- scripts/jobRunner.js
+++ scripts/jobRunner.js
@@ -8,21 +8,33 @@ const PROCESS_NAME = `jobRunner-${process.pid}`
 const MAX_JOB_RUNTIME = 60 * 60 * 4 * 1000 // 4 hours
 const adapter = new PrismaAdapter({ db })
 
+const log = (message, job) => {
+  const { handler, args } = JSON.parse(job.handler)
+
+  console.info(`[${handler}] ${message} ${job.id}`, {
+    args: args,
+  })
+}
+
 // Actually instantiate the job class and call `perform()` on it, passing in
 // any args
 const perform = async (job) => {
+  log('Started', job)
+
   const details = JSON.parse(job.handler)
   const Job = await import(`api/src/jobs/${details.handler}`)
   await new Job[details.handler]().perform(...details.args)
 }
 
 // Handle job success: remove from DB
-const onSuccess = async (job) => {
+const success = async (job) => {
+  log('Complete', job)
   await adapter.succeed(job)
 }
 
 // Handle job failure: add error to DB and retry time (or mark `failedAt`)
-const onFailure = async (job, error) => {
+const failure = async (job, error) => {
+  log('Failed', job)
   await adapter.fail(job, error)
 }
 
@@ -35,22 +47,11 @@ export default async () => {
     })
 
     if (job) {
-      const { handler, args } = JSON.parse(job.handler)
-
       try {
-        console.info(`[${handler}] Started ${job.id}`, {
-          args: args,
-        })
         await perform(job)
-        console.info(`[${handler}] Complete ${job.id}`, {
-          args: args,
-        })
-        await onSuccess(job)
+        await success(job)
       } catch (e) {
-        console.error(`[${handler}] Failed ${job.id}`, {
-          args: args,
-        })
-        await onFailure(job, e)
+        await failure(job, e)
       }
     }
 
