039b776d7038f7859c4f41f5229b60102bd010a0
Rob Cameron <cannikin@fastmail.com>
2024-06-13 15:00:50 -0700

    Simplify job to just delay and output console message

--- api/src/jobs/ProductBackorderJob.js
+++ api/src/jobs/ProductBackorderJob.js
@@ -4,33 +4,37 @@ import { ProductBackorderEmail } from 'src/mail/ProductBackorderEmail'
 
 import { RedwoodJob } from './RedwoodJob'
 
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
 export class ProductBackorderJob extends RedwoodJob {
   // static queue = 'email'
   // statis priority = 10
 
   async perform(productID) {
-    const product = await db.product.findUnique({ where: { id: productID } })
+    console.info(`Job running, checking product ${productID} for backorders...`)
+    await delay(2000)
+    // const product = await db.product.findUnique({ where: { id: productID } })
 
-    if (product.inventoryCount > 0) {
-      const waitList = await db.waitlist.findMany({
-        where: { productId: product.id },
-      })
+    // if (product.inventoryCount > 0) {
+    //   const waitList = await db.waitlist.findMany({
+    //     where: { productId: product.id },
+    //   })
 
-      const sent = []
+    //   const sent = []
 
-      for (const user of waitList) {
-        sent.push(
-          await mailer.send(ProductBackorderEmail({ product }), {
-            to: user.email,
-            subject: `Product Back in Stock: ${product.name}`,
-          })
-        )
-        await db.waitlist.delete({
-          where: { productId: product.id, email: user.email },
-        })
-      }
+    //   for (const user of waitList) {
+    //     sent.push(
+    //       await mailer.send(ProductBackorderEmail({ product }), {
+    //         to: user.email,
+    //         subject: `Product Back in Stock: ${product.name}`,
+    //       })
+    //     )
+    //     await db.waitlist.delete({
+    //       where: { productId: product.id, email: user.email },
+    //     })
+    //   }
 
-      console.log(`[ProductBackorderJob]: Sent ${sent.length} waitlist emails`)
-    }
+    //   console.log(`[ProductBackorderJob]: Sent ${sent.length} waitlist emails`)
+    // }
   }
 }
