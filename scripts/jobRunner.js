b17500ced9f8e1d3ba55a3ead5465db569e22805
Rob Cameron <cannikin@fastmail.com>
2024-06-17 09:38:41 -0700

    Breakout job stages into separate functions

--- scripts/jobRunner.js
+++ scripts/jobRunner.js
@@ -6,55 +6,93 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 const WAIT_TIME = 5000
 const PROCESS_NAME = `jobRunner-${process.pid}`
 const MAX_JOB_RUNTIME = 60 * 60 * 4 * 1000 // 4 hours
-const adapter = new PrismaAdapter({ db })
 
-const log = (message, job) => {
-  const { handler, args } = JSON.parse(job.handler)
+// TODO: Create job executor class
+class Executor {
+  constructor(options) {
+    this.options = options
+    this.adapter = options?.adapter
+    this.job = options?.job
 
-  console.info(`[${handler}] ${message} ${job.id}`, {
-    args: args,
-  })
+    // TODO validate that everything we need to create the executor is present
+  }
+
+  log(message) {
+    const { handler, args } = JSON.parse(this.job.handler)
+
+    console.info(`[${handler}] ${message} ${this.job.id}`, {
+      args: args,
+    })
+  }
+  // Actually instantiate the job class and call `perform()` on it, passing in
+  // any args
+  async perform(job) {
+    this.log('Started', job)
+
+    try {
+      const details = JSON.parse(this.job.handler)
+      const Job = await import(`api/src/jobs/${details.handler}`)
+      new Job[details.handler]().perform(...details.args)
+
+      this.#success()
+    } catch (e) {
+      this.#failure(e)
+    }
+  }
+
+  // Handle job success: remove from DB
+  #success() {
+    this.log('Complete')
+    this.adapter.success(this.job)
+  }
+
+  // Handle job failure: add error to DB and retry time (or mark `failedAt`)
+  #failure(error) {
+    this.log('Failed')
+    this.adapter.failure(this.job, error)
+  }
 }
 
-// Actually instantiate the job class and call `perform()` on it, passing in
-// any args
-const perform = async (job) => {
-  log('Started', job)
+class Worker {
+  constructor(options) {
+    this.options = options
+    this.adapter = options?.adapter
+    this.queue = options?.queue
+    this.processName = options?.PROCESS_NAME
+    this.maxRuntime = options?.MAX_JOB_RUNTIME
 
-  const details = JSON.parse(job.handler)
-  const Job = await import(`api/src/jobs/${details.handler}`)
-  await new Job[details.handler]().perform(...details.args)
+    // TODO: check that everything we need to create the worker is present (adapter)
+  }
+
+  async run() {
+    // Trick to run forever, as the linter doesn't like `while (true)`
+    for (;;) {
+      const job = await this.adapter.find({
+        processName: PROCESS_NAME,
+        maxRuntime: MAX_JOB_RUNTIME,
+      })
+
+      if (job) {
+        await new Executor({ adapter: this.adapter, job }).perform()
+      }
+
+      await delay(WAIT_TIME)
+    }
+  }
 }
 
-// Handle job success: remove from DB
-const success = async (job) => {
-  log('Complete', job)
-  await adapter.succeed(job)
-}
-
-// Handle job failure: add error to DB and retry time (or mark `failedAt`)
-const failure = async (job, error) => {
-  log('Failed', job)
-  await adapter.fail(job, error)
-}
+// This script simply initializes a worker that runs forever. An extended
+// version of the script could start N workers in parallel that only work
+// off a single queue
 
 export default async () => {
-  // Trick to run forever, as the linter doesn't like `while (true)`
-  for (;;) {
-    const job = await adapter.find({
-      processName: PROCESS_NAME,
-      maxRuntime: MAX_JOB_RUNTIME,
-    })
+  const queue = 'default'
+  const adapter = new PrismaAdapter({ db })
 
-    if (job) {
-      try {
-        await perform(job)
-        await success(job)
-      } catch (e) {
-        await failure(job, e)
-      }
-    }
-
-    await delay(WAIT_TIME)
-  }
+  new Worker({
+    adapter,
+    queue,
+    processName: PROCESS_NAME,
+    maxRuntime: MAX_JOB_RUNTIME,
+  }).run()
 }
