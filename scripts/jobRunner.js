8e5de71b61ba5948690272b9ae537f9534a0a187
Rob Cameron <cannikin@fastmail.com>
2024-06-19 16:34:01 -0700

    Delete jobRunner exec script

--- scripts/jobRunner.js
+++
@@ -1,8 +0,0 @@
-import { PrismaAdapter } from 'api/src/jobs/PrismaAdapter'
-import { db } from 'api/src/lib/db'
-
-import { Worker } from './Worker'
-
-export default async () => {
-  new Worker({ adapter: new PrismaAdapter({ db }) }).run()
-}
