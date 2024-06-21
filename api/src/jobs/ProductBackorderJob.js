088a881cde6a71dbe0648e3cefefad948afb69c2
Rob Cameron <cannikin@fastmail.com>
2024-06-21 11:06:55 -0700

    Fill out sample job info

--- api/src/jobs/ProductBackorderJob.js
+++ api/src/jobs/ProductBackorderJob.js
@@ -1,7 +1,3 @@
-// import { db } from 'src/lib/db'
-// import { mailer } from 'src/lib/mailer'
-// import { ProductBackorderEmail } from 'src/mail/ProductBackorderEmail'
-
 import { RedwoodJob } from './RedwoodJob'
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
@@ -17,26 +13,34 @@ export class ProductBackorderJob extends RedwoodJob {
   }
 }
 
-// const product = await db.product.findUnique({ where: { id: productID } })
-
-// if (product.inventoryCount > 0) {
-//   const waitList = await db.waitlist.findMany({
-//     where: { productId: product.id },
-//   })
-
-//   const sent = []
-
-//   for (const user of waitList) {
-//     sent.push(
-//       await mailer.send(ProductBackorderEmail({ product }), {
-//         to: user.email,
-//         subject: `Product Back in Stock: ${product.name}`,
-//       })
-//     )
-//     await db.waitlist.delete({
-//       where: { productId: product.id, email: user.email },
-//     })
+// Actual sample job implementation that sends an email to users on a waitlist
+//
+// import { db } from 'src/lib/db'
+// import { mailer } from 'src/lib/mailer'
+// import { ProductBackorderEmail } from 'src/mail/ProductBackorderEmail'
+//
+// export class ProductBackorderJob extends RedwoodJob {
+//   async perform(productID) {
+//
+//    const product = await db.product.findUnique({ where: { id: productID } })
+//
+//    if (product.inventoryCount > 0) {
+//      const waitList = await db.waitlist.findMany({
+//        where: { productId: product.id },
+//      })
+//
+//      const sent = []
+//
+//      for (const user of waitList) {
+//        sent.push(
+//          await mailer.send(ProductBackorderEmail({ product }), {
+//            to: user.email,
+//            subject: `Product Back in Stock: ${product.name}`,
+//          })
+//        )
+//        await db.waitlist.delete({
+//          where: { productId: product.id, email: user.email },
+//        })
+//      }
 //   }
-
-//   console.log(`[ProductBackorderJob]: Sent ${sent.length} waitlist emails`)
 // }
