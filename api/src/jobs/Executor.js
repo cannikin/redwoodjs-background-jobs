815d890a57e32683806d4804aa8df9c3d13e537f
Rob Cameron <cannikin@fastmail.com>
2024-06-21 11:03:21 -0700

    Pass logger to Executor, add stack trace to errors

--- api/src/jobs/Executor.js
+++ api/src/jobs/Executor.js
@@ -1,3 +1,5 @@
+// Used by the job runner to execute a job and track success or failure
+
 import { AdapterRequiredError, JobRequiredError } from 'src/jobs/errors'
 
 export class Executor {
@@ -5,17 +7,24 @@ export class Executor {
     this.options = options
     this.adapter = options?.adapter
     this.job = options?.job
+    this.logger = options?.logger
 
     if (!this.adapter) throw new AdapterRequiredError()
     if (!this.job) throw new JobRequiredError()
   }
 
-  #log(message) {
+  #log(message, stack) {
     const { handler, args } = JSON.parse(this.job.handler)
-
-    console.info(`[${handler}] ${message} ${this.job.id}`, {
-      args: args,
-    })
+    const parts = [
+      `[${handler}] ${message} ${this.job.id}`,
+      {
+        args: args,
+      },
+    ]
+    if (stack) {
+      parts.push(stack)
+    }
+    this.logger.info(...parts)
   }
 
   // Actually instantiate the job class and call `perform()` on it, passing in
@@ -30,14 +39,14 @@ export class Executor {
 
       this.#success()
     } catch (e) {
-      console.info(e)
+      this.#log(e.message, e.stack)
       this.#failure(e)
     }
   }
 
   // Handle job success: remove from DB
   #success() {
-    this.#log('Complete')
+    this.#log('Success')
     this.adapter.success(this.job)
   }
 
