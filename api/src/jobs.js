7c456c9a1b0383a86390b6bb7643c502509c5765
Rob Cameron <cannikin@fastmail.com>
2024-06-24 09:48:18 -0700

    Functionify jobs script

--- api/src/jobs.js
+++ api/src/jobs.js
@@ -12,155 +12,203 @@ loadEnvFiles()
 
 process.title = 'rw-job-runner'
 
-const argv = yargs(hideBin(process.argv))
-  .usage(
-    'Starts the RedwoodJob runner to process background jobs\n\nUsage: $0 <command> [options]'
-  )
-  .command('work', 'Start a worker and process jobs')
-  .command('workoff', 'Start a worker and exit after all jobs processed')
-  .command('start', 'Start workers in daemon mode', (yargs) => {
-    yargs
-      .option('n', {
-        type: 'string',
-        describe:
-          'Number of workers to start OR queue:num pairs of workers to start (see examples)',
-        default: '1',
-      })
-      .example(
-        '$0 start -n 2',
-        'Start the job runner with 2 workers in daemon mode'
-      )
-      .example(
-        '$0 start -n default:2,email:1',
-        'Start the job runner in daemon mode with 2 workers for the "default" queue and 1 for the "email" queue'
-      )
-  })
-  .command('stop', 'Stop any daemonized job workers')
-  .command('restart', 'Stop and start any daemonized job workers', (yargs) => {
-    yargs
-      .option('n', {
-        type: 'string',
-        describe:
-          'Number of workers to start OR queue:num pairs of workers to start (see examples)',
-        default: '1',
-      })
-      .example(
-        '$0 restart -n 2',
-        'Restart the job runner with 2 workers in daemon mode'
-      )
-      .example(
-        '$0 restart -n default:2,email:1',
-        'Restart the job runner in daemon mode with 2 workers for the `default` queue and 1 for the `email` queue'
-      )
-  })
-  .command('clear', 'Clear the job queue')
-  .demandCommand(1, 'You must specify a mode to start in')
-  .example(
-    '$0 start -n 2',
-    'Start the job runner with 2 workers in daemon mode'
-  )
-  .example(
-    '$0 start -n default:2,email:1',
-    'Start the job runner in daemon mode with 2 workers for the "default" queue and 1 for the "email" queue'
-  )
-  .help().argv
+const parseArgs = () => {
+  const argv = yargs(hideBin(process.argv))
+    .usage(
+      'Starts the RedwoodJob runner to process background jobs\n\nUsage: $0 <command> [options]'
+    )
+    .command('work', 'Start a worker and process jobs')
+    .command('workoff', 'Start a worker and exit after all jobs processed')
+    .command('start', 'Start workers in daemon mode', (yargs) => {
+      yargs
+        .option('n', {
+          type: 'string',
+          describe:
+            'Number of workers to start OR queue:num pairs of workers to start (see examples)',
+          default: '1',
+        })
+        .example(
+          '$0 start -n 2',
+          'Start the job runner with 2 workers in daemon mode'
+        )
+        .example(
+          '$0 start -n default:2,email:1',
+          'Start the job runner in daemon mode with 2 workers for the "default" queue and 1 for the "email" queue'
+        )
+    })
+    .command('stop', 'Stop any daemonized job workers')
+    .command(
+      'restart',
+      'Stop and start any daemonized job workers',
+      (yargs) => {
+        yargs
+          .option('n', {
+            type: 'string',
+            describe:
+              'Number of workers to start OR queue:num pairs of workers to start (see examples)',
+            default: '1',
+          })
+          .example(
+            '$0 restart -n 2',
+            'Restart the job runner with 2 workers in daemon mode'
+          )
+          .example(
+            '$0 restart -n default:2,email:1',
+            'Restart the job runner in daemon mode with 2 workers for the `default` queue and 1 for the `email` queue'
+          )
+      }
+    )
+    .command('clear', 'Clear the job queue')
+    .demandCommand(1, 'You must specify a mode to start in')
+    .example(
+      '$0 start -n 2',
+      'Start the job runner with 2 workers in daemon mode'
+    )
+    .example(
+      '$0 start -n default:2,email:1',
+      'Start the job runner in daemon mode with 2 workers for the "default" queue and 1 for the "email" queue'
+    )
+    .help().argv
 
-const command = argv._[0]
-const shouldDetachWorkers = ['start', 'restart'].includes(command)
-
-// Builds up an array of arrays, with queue name and id:
-//   `-n default:2,email:1` => [ ['default', 0], ['default', 1], ['email', 0] ]
-// If only given a number of workers then queue name is an empty string:
-//   `-n 2` => [ ['', 0], ['', 1] ]
-let workers = []
-
-// default to one worker for commands that don't specify
-if (!argv.n) {
-  argv.n = '1'
+  return { numWorkers: argv.n, command: argv._[0] }
 }
 
-// if only a number was given, convert it to a nameless worker: `2` => `:2`
-if (!isNaN(parseInt(argv.n))) {
-  argv.n = `:${argv.n}`
-}
+const getWorkerConfig = (numWorkers) => {
+  // Builds up an array of arrays, with queue name and id:
+  //   `-n default:2,email:1` => [ ['default', 0], ['default', 1], ['email', 0] ]
+  // If only given a number of workers then queue name is an empty string:
+  //   `-n 2` => [ ['', 0], ['', 1] ]
+  let workers = []
 
-// split the queue:num pairs and build the workers array
-argv.n.split(',').forEach((count) => {
-  const [queue, num] = count.split(':')
-  for (let i = 0; i < parseInt(num); i++) {
-    workers.push([queue || null, i])
-  }
-})
-
-const children = []
-
-// Until we figure out Redwood's logger, use console for output
-// const logger = console
-
-logger.warn(
-  `Starting RedwoodJob Runner at ${new Date().toISOString()} with ${workers.length} worker(s)...`
-)
-
-// Create a child process for every worker
-workers.forEach(([queue, id], i) => {
-  // list of args to send to the forked worker script
-  const workerArgs = ['-i', id]
-
-  // add the queue name if present
-  if (queue) {
-    workerArgs.push('-q', queue)
+  // default to one worker for commands that don't specify
+  if (!numWorkers) {
+    numWorkers = '1'
   }
 
-  // are we in workoff mode?
-  if (argv._[0] === 'workoff') {
-    workerArgs.push('-o')
+  // if only a number was given, convert it to a nameless worker: `2` => `:2`
+  if (!isNaN(parseInt(numWorkers))) {
+    numWorkers = `:${numWorkers}`
   }
 
-  // fork the worker process
-  const child = fork('api/dist/worker.js', workerArgs, {
-    detached: shouldDetachWorkers,
-    stdio: shouldDetachWorkers ? 'ignore' : 'inherit',
+  // split the queue:num pairs and build the workers array
+  numWorkers.split(',').forEach((count) => {
+    const [queue, num] = count.split(':')
+    for (let i = 0; i < parseInt(num); i++) {
+      workers.push([queue || null, i])
+    }
   })
 
-  if (shouldDetachWorkers) {
-    child.unref()
-  } else {
-    // children stay attached so watch for their exit
-    child.on('exit', (code) => {
-      logger.info(`[${process.title}] Exited with code ${code}`)
-      children.splice(children.indexOf(child), 1)
+  return workers
+}
+
+const startWorkers = ({ workerConfig, isDetached, workoff = false }) => {
+  logger.warn(`Starting ${workerConfig.length} worker(s)...`)
+
+  const processes = []
+
+  return workerConfig.map(([queue, id], i) => {
+    // list of args to send to the forked worker script
+    const workerArgs = ['-i', id]
+
+    // add the queue name if present
+    if (queue) {
+      workerArgs.push('-q', queue)
+    }
+
+    // are we in workoff mode?
+    if (workoff) {
+      workerArgs.push('-o')
+    }
+
+    // fork the worker process
+    const worker = fork('api/dist/worker.js', workerArgs, {
+      detached: isDetached,
+      stdio: isDetached ? 'ignore' : 'inherit',
     })
 
-    // track our array of workers so we can send them all messages
-    children.push(child)
-  }
-})
+    if (isDetached) {
+      worker.unref()
+    } else {
+      // children stay attached so watch for their exit
+      worker.on('exit', (code) => {
+        logger.info(`[${worker.title}] Exited with code ${code}`)
+        processes.splice(processes.indexOf(worker), 1)
+      })
+    }
 
-// We're running in start or restart mode, so just exit this parent process
-if (shouldDetachWorkers) {
+    return worker
+  })
+}
+
+const signalSetup = (workers) => {
+  // if we get here then we're still monitoring workers and have to pass on signals
+  let sigtermCount = 0
+
+  // If the parent receives a ctrl-c, tell each worker to gracefully exit.
+  // If the parent receives a second ctrl-c, exit immediately.
+  process.on('SIGINT', () => {
+    sigtermCount++
+    let message =
+      'SIGINT received: shutting down workers gracefully (press Ctrl-C again to exit immediately)...'
+
+    if (sigtermCount > 1) {
+      message = 'SIGINT received again, exiting immediately...'
+    }
+
+    logger.info(message)
+
+    workers.forEach((worker) => {
+      sigtermCount > 1 ? worker.kill() : worker.kill('SIGINT')
+    })
+  })
+}
+
+const exitWithDetachNotice = () => {
   logger.warn(
     `Workers detached, exiting parent process at ${new Date().toISOString()}.`
   )
   process.exit(0)
 }
 
-// if we get here then we're still monitoring children and have to pass on signals
-let sigtermCount = 0
+const stopWorkers = ({ workerConfig }) => {
+  logger.warn(`Stopping ${workerConfig.length} worker(s)...`)
+}
 
-// If the parent receives a ctrl-c, tell each worker to gracefully exit.
-// If the parent receives a second ctrl-c, exit immediately.
-process.on('SIGINT', () => {
-  sigtermCount++
-  let message =
-    'SIGINT received: shutting down workers gracefully (press Ctrl-C again to exit immediately)...'
+const clearQueue = () => {
+  logger.warn(`Clearing job queue...`)
+}
 
-  if (sigtermCount > 1) {
-    message = 'SIGINT received again, exiting immediately...'
+const main = () => {
+  const { numWorkers, command, isDetached } = parseArgs()
+  const workerConfig = getWorkerConfig(numWorkers)
+
+  logger.warn(`Starting RedwoodJob Runner at ${new Date().toISOString()}...`)
+
+  switch (command) {
+    case 'start':
+      startWorkers({ workerConfig, isDetatched: true })
+      exitWithDetachNotice()
+      break
+    case 'restart':
+      stopWorkers()
+      startWorkers({ workerConfig, isDetached })
+      exitWithDetachNotice()
+      break
+    case 'work':
+      signalSetup(startWorkers({ workerConfig, isDetatched: false }))
+      break
+    case 'workoff':
+      signalSetup(
+        startWorkers({ workerConfig, isDetatched: false, workoff: true })
+      )
+      break
+    case 'stop':
+      stopWorkers(workerConfig)
+      break
+    case 'clear':
+      clearQueue()
+      break
   }
+}
 
-  logger.info(message)
-
-  children.forEach((worker) => {
-    sigtermCount > 1 ? worker.kill() : worker.kill('SIGINT')
-  })
-})
+main()
