ff717213c886049589759d397837e59f14068ceb
Rob Cameron <cannikin@fastmail.com>
2024-06-24 09:05:24 -0700

    Move imports, update log output

--- api/src/worker.js
+++ api/src/worker.js
@@ -4,6 +4,12 @@ import { hideBin } from 'yargs/helpers'
 import yargs from 'yargs/yargs'
 
 import { loadEnvFiles } from '@redwoodjs/cli/dist/lib/loadEnvFiles'
+
+import { PrismaAdapter } from './jobs/PrismaAdapter'
+import { Worker } from './jobs/Worker'
+import { db } from './lib/db'
+import { logger } from './lib/logger.js'
+
 loadEnvFiles()
 
 const argv = yargs(hideBin(process.argv))
@@ -29,14 +35,7 @@ const argv = yargs(hideBin(process.argv))
   })
   .help().argv
 
-import { PrismaAdapter } from './jobs/PrismaAdapter'
-import { Worker } from './jobs/Worker'
-import { db } from './lib/db'
-
 const TITLE_PREFIX = `rw-job-worker`
-
-// TODO figure out Redwood logger
-const logger = console
 
 // set the process title
 let title = TITLE_PREFIX
@@ -47,7 +46,7 @@ if (argv.q) {
 }
 process.title = title
 
-logger.info(`[${process.title}] Starting Worker...`)
+logger.info({ worker: process.title }, `Starting Worker...`)
 
 const worker = new Worker({
   adapter: new PrismaAdapter({ db }),
@@ -61,7 +60,7 @@ const worker = new Worker({
 // is set to `false`, or the worker receives a SIGINT), we'll send a message and
 // exit gracefully
 worker.run().then(() => {
-  logger.info(`[${process.title}] Worker finished, shutting down.`)
+  logger.info({ worker: process.title }, `Worker finished, shutting down.`)
   process.exit(0)
 })
 
@@ -71,7 +70,7 @@ worker.run().then(() => {
 // workers will exit gracefully by setting `forever` to `false` which will tell
 // it not to pick up a new job when done with the current one
 process.on('SIGINT', () => {
-  logger.info(`[${process.title}] SIGINT received, finishing work...`)
+  logger.info({ worker: process.title }, `SIGINT received, finishing work...`)
   worker.forever = false
 })
 
@@ -79,6 +78,6 @@ process.on('SIGINT', () => {
 // instead in which case we exit immediately no matter what state the worker is
 // in
 process.on('SIGTERM', () => {
-  logger.info(`[${process.title}] SIGTERM received, exiting now!`)
+  logger.info({ worker: process.title }, `SIGTERM received, exiting now!`)
   process.exit(0)
 })
