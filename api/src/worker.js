4c2a6af728f72fd173162d9f98ce1829d86b48c5
Rob Cameron <cannikin@fastmail.com>
2024-06-25 12:14:28 -0700

    Refactor worker.js to use functions

--- api/src/worker.js
+++ api/src/worker.js
@@ -4,98 +4,105 @@ import { hideBin } from 'yargs/helpers'
 import yargs from 'yargs/yargs'
 
 import { loadEnvFiles } from '@redwoodjs/cli/dist/lib/loadEnvFiles'
+loadEnvFiles()
 
 import { PrismaAdapter } from './jobs/PrismaAdapter'
 import { Worker } from './jobs/Worker'
 import { db } from './lib/db'
 import { logger } from './lib/logger.js'
 
-loadEnvFiles()
-
-const argv = yargs(hideBin(process.argv))
-  .usage(
-    'Starts a single RedwoodJob worker to process background jobs\n\nUsage: $0 [options]'
-  )
-  .option('i', {
-    alias: 'id',
-    type: 'number',
-    description: 'The worker ID',
-    default: 0,
-  })
-  .option('q', {
-    alias: 'queue',
-    type: 'string',
-    description: 'The named queue to work on',
-  })
-  .option('o', {
-    alias: 'workoff',
-    type: 'boolean',
-    default: false,
-    description: 'Work off all jobs in the queue and exit',
-  })
-  .option('c', {
-    alias: 'clear',
-    type: 'boolean',
-    default: false,
-    description: 'Remove all jobs in the queue and exit',
-  })
-  .help().argv
-
 const TITLE_PREFIX = `rw-job-worker`
 
-// set the process title
-let title = TITLE_PREFIX
-if (argv.q) {
-  title += `.${argv.queue}.${argv.id}`
-} else {
-  title += `.${argv.id}`
+const parseArgs = (argv) => {
+  return yargs(hideBin(argv))
+    .usage(
+      'Starts a single RedwoodJob worker to process background jobs\n\nUsage: $0 [options]'
+    )
+    .option('i', {
+      alias: 'id',
+      type: 'number',
+      description: 'The worker ID',
+      default: 0,
+    })
+    .option('q', {
+      alias: 'queue',
+      type: 'string',
+      description: 'The named queue to work on',
+    })
+    .option('o', {
+      alias: 'workoff',
+      type: 'boolean',
+      default: false,
+      description: 'Work off all jobs in the queue and exit',
+    })
+    .option('c', {
+      alias: 'clear',
+      type: 'boolean',
+      default: false,
+      description: 'Remove all jobs in the queue and exit',
+    })
+    .help().argv
 }
-process.title = title
 
-logger.info(
-  { worker: process.title },
-  `Starting work at ${new Date().toISOString()}...`
-)
+const setProcessTitle = ({ id, queue }) => {
+  // set the process title
+  let title = TITLE_PREFIX
+  if (queue) {
+    title += `.${queue}.${id}`
+  } else {
+    title += `.${id}`
+  }
+  process.title = title
+}
 
-// A worker actually does stuff, so start it turning command line flags into
-// args
-const worker = new Worker({
-  adapter: new PrismaAdapter({ db }),
-  processName: process.title,
-  logger,
-  queue: argv.queue,
-  workoff: argv.workoff,
-  clear: argv.clear,
-})
+const setupSignals = (worker) => {
+  // if the parent itself receives a ctrl-c it'll pass that to the workers.
+  // workers will exit gracefully by setting `forever` to `false` which will tell
+  // it not to pick up a new job when done with the current one
+  process.on('SIGINT', () => {
+    logger.warn(
+      { worker: process.title },
+      `SIGINT received at ${new Date().toISOString()}, finishing work...`
+    )
+    worker.forever = false
+  })
 
-// run() normally loops forever, but if it does stop (because `worker.forever`
-// is set to `false`, or the worker receives a SIGINT), we'll send a message and
-// exit gracefully
-worker.run().then(() => {
-  logger.info({ worker: process.title }, `Worker finished, shutting down.`)
-  process.exit(0)
-})
+  // if the parent itself receives a ctrl-c more than once it'll send SIGTERM
+  // instead in which case we exit immediately no matter what state the worker is
+  // in
+  process.on('SIGTERM', () => {
+    logger.info(
+      { worker: process.title },
+      `SIGTERM received at ${new Date().toISOString()}, exiting now!`
+    )
+    process.exit(0)
+  })
+}
 
-// watch for signals from the parent
+const main = async () => {
+  const { id, queue, clear, workoff } = parseArgs(process.argv)
+  setProcessTitle({ id, queue })
 
-// if the parent itself receives a ctrl-c it'll pass that to the workers.
-// workers will exit gracefully by setting `forever` to `false` which will tell
-// it not to pick up a new job when done with the current one
-process.on('SIGINT', () => {
-  logger.warn(
-    { worker: process.title },
-    `SIGINT received at ${new Date().toISOString()}, finishing work...`
-  )
-  worker.forever = false
-})
-
-// if the parent itself receives a ctrl-c more than once it'll send SIGTERM
-// instead in which case we exit immediately no matter what state the worker is
-// in
-process.on('SIGTERM', () => {
   logger.info(
     { worker: process.title },
-    `SIGTERM received at ${new Date().toISOString()}, exiting now!`
+    `Starting work at ${new Date().toISOString()}...`
   )
-  process.exit(0)
-})
+
+  const worker = new Worker({
+    adapter: new PrismaAdapter({ db }),
+    processName: process.title,
+    logger,
+    queue,
+    workoff,
+    clear,
+  })
+
+  worker.run().then(() => {
+    logger.info({ worker: process.title }, `Worker finished, shutting down.`)
+    process.exit(0)
+  })
+
+  setupSignals(worker)
+}
+
+main()
