082582fc368d394129f0643c7b5a7a8ecdcf3674
Rob Cameron <cannikin@fastmail.com>
2024-06-26 15:36:47 -0700

    Adds sample welcome email job and function

---
+++ api/src/jobs/WelcomeEmailJob.js
@@ -0,0 +1,15 @@
+import { RedwoodJob } from './RedwoodJob'
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+export class WelcomeEmailJob extends RedwoodJob {
+  static queue = 'email'
+
+  async perform(email) {
+    const wait = Math.round(Math.random() * 1000 * 2)
+    this.logger.info(
+      `Sending welcome email to ${email} (delaying ${wait}ms)...`
+    )
+    await delay(wait)
+  }
+}
